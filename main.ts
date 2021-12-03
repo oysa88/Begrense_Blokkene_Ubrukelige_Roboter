763e8f9f12343f8a9495ea0024205c43ed87055a
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2021-12-03 13:16:27 +0100

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,30 @@
+let variabel = 0
 input.onButtonPressed(Button.A, function () {
-	
+    if (randint(0, 10) == 0 + 0) {
+        variabel = 0
+    }
+    bitbot.bbSetServo(BBServos.P1, 90)
+    bitbot.bb360Servo(BBServos.P1, BBDirection.Forward, 50)
+    bitbot.bbServoTrim(BBServos.P1, 5)
+    bitbot.bbStopServos()
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    bitbot.setLedColor(0xFF0000)
+    bitbot.ledClear()
+    bitbot.setPixelColor(0, 0xFF0000)
+    bitbot.ledRotate()
+    basic.showNumber(0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Hello!")
+    basic.clearScreen()
 })
-let variabel = 0
-bitbot.setLedColor(0xFF0000)
-bitbot.ledClear()
-bitbot.setPixelColor(0, 0xFF0000)
-bitbot.ledRotate()
-basic.showNumber(0)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showIcon(IconNames.Heart)
-basic.showString("Hello!")
-basic.clearScreen()
-basic.pause(100)
-if (randint(0, 10) == 0 + 0) {
-    variabel = 0
-}
-bitbot.bbSetServo(BBServos.P1, 90)
-bitbot.bb360Servo(BBServos.P1, BBDirection.Forward, 50)
-bitbot.bbServoTrim(BBServos.P1, 5)
-bitbot.bbStopServos()
 basic.forever(function () {
-	
+    basic.pause(100)
 })
